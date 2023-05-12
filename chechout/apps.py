0d6ef2feca4382cb443b5bc767c26dd4e1e06830
Dmytro Martynenko <dmytromartynenko1@gmail.com>
2023-05-12 20:07:25 +0000

    Add secure views and template

--- chechout/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class ChechoutConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'chechout'
-
-    def ready(self):
-        import checkout.signals
