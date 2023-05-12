d4b254ec8aeb64dee88424a398275137cdc35af4
Dmytro Martynenko <dmytromartynenko1@gmail.com>
2023-05-12 14:17:12 +0000

    Added forms, admin and signals

--- chechout/apps.py
+++ chechout/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class ChechoutConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'chechout'
+
+    def ready(self):
+        import checkout.signals
